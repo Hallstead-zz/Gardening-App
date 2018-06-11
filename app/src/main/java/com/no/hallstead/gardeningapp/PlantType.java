0248497ce02885ec97ad8358d2a2310a3fd1feb1
serico7 <35274836+serico7@users.noreply.github.com>
2018-06-11 14:34:57 -0600

    Wrote PlantType according to UML

--- app/src/main/java/com/no/hallstead/gardeningapp/PlantType.java
+++ app/src/main/java/com/no/hallstead/gardeningapp/PlantType.java
@@ -1,4 +1,25 @@
 package com.no.hallstead.gardeningapp;
 
 public class PlantType {
+    private String name;
+    private int waterFreq;
+    private int timeToHarvest;
+
+    public PlantType(String name, int waterFreq, int timeToHarvest) {
+        this.name = name;
+        this.waterFreq = waterFreq;
+        this.timeToHarvest = timeToHarvest;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getWaterFreq() {
+        return waterFreq;
+    }
+
+    public int getTimeToHarvest() {
+        return timeToHarvest;
+    }
 }
