8927287cf445f16d943ba72c447cbe52d28a1cd5
serico7 <35274836+serico7@users.noreply.github.com>
2018-06-12 02:12:24 -0600

    Added the Plant class methods according to the UML. Date doesn't work for what we need to do, GregorianCalender does it better and has better documentation.

--- app/src/main/java/com/no/hallstead/gardeningapp/Plant.java
+++ app/src/main/java/com/no/hallstead/gardeningapp/Plant.java
@@ -1,4 +1,41 @@
 package com.no.hallstead.gardeningapp;
 
+import java.util.GregorianCalendar;
+
 public class Plant {
+    private String name;
+    private PlantType type;
+    private GregorianCalendar datePlanted;
+    private GregorianCalendar dateToHarvest;
+    private GregorianCalendar dateLastWatered;
+    private boolean needsWater;
+    private boolean needsHarvest;
+
+    public Plant(PlantType type, GregorianCalendar datePlanted) {
+        name = type.getName();
+        this.type = type;
+        this.datePlanted = datePlanted;
+        dateToHarvest = new GregorianCalendar();
+        dateToHarvest.add(dateToHarvest.DAY_OF_MONTH, type.getTimeToHarvest());
+        dateLastWatered = new GregorianCalendar();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public GregorianCalendar getDatePlanted() {
+        return datePlanted;
+    }
+
+    public void water() {
+        
+    }
+
+    public void harvest() {
+        return;
+    }
+
+
+
 }
