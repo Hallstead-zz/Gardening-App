09258e7c916f67a38f1041c25773204b769b152b
Hallstead <Hallstead@users.noreply.github.com>
2018-06-18 14:31:16 -0600

    renaming files

--- app/src/main/java/com/no/hallstead/gardeningapp/AddPlantToPlot.java
+++ app/src/main/java/com/no/hallstead/gardeningapp/AddPlantToPlot.java
@@ -7,12 +7,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class SelectPlant extends AppCompatActivity {
+public class AddPlantToPlot extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_select_plant);
+        setContentView(R.layout.activity_add_plant_to_plot);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
