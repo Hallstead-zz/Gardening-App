1138d2b0800b36d9ad67903b8148b10449d02df7
Bluelight413 <was16006@byui.edu>
2018-06-27 15:01:03 -0600

    Added a log.i to AddPlantToPlot

--- app/src/main/java/com/no/hallstead/gardeningapp/AddPlantToPlot.java
+++ app/src/main/java/com/no/hallstead/gardeningapp/AddPlantToPlot.java
@@ -9,6 +9,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Spinner;
@@ -90,6 +91,8 @@ public class AddPlantToPlot extends AppCompatActivity {
         String mainPlant = gson.toJson(newPlant, Plant.class);
         editor.putString(("Plant" + plotLocation), mainPlant);
         editor.apply();
+
+        Log.i("AddPlantToPlot","Planted a " + plantName + " at " + plotLocation);
 
         //Informs the user that the Plant was added and exits
         Toast toast = Toast.makeText(context, "Plant was added!", duration);
